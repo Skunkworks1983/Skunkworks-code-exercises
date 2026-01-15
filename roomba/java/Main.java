35ebb22cd6fd0ba81de2cfe60e293bc59e332129
Jack Alexander <jalexander.qed@gmail.com>
2026-01-14 20:07:47 -0800

    Make the grid 20X20

--- roomba/java/Main.java
+++ roomba/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Grid grid = new Grid(100, 100); //Not big enough...
+        Grid grid = new Grid(20, 20);
         grid.print();
         grid.sleep();
 

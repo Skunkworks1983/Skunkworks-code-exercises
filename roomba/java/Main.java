db761f1235548ea4a447609e0d4a2e9ada8a3b34
Jack Alexander <jalexander.qed@gmail.com>
2026-01-14 19:42:21 -0800

    Make the robot move only south.

--- roomba/java/Main.java
+++ roomba/java/Main.java
@@ -13,23 +13,8 @@ public class Main {
 
     public static void cleanFloor(Grid grid) {
         while (true) {
-            int stepType = step % 4;
-            switch (stepType) {
-                case 0:
-                    grid.robotMoveSouth();
-                    break;
-                case 1:
-                    grid.robotMoveEast();
-                    break;
-                case 2:
-                    grid.robotMoveNorth();
-                    break;
-                case 3:
-                    grid.robotMoveWest();
-                    break;
-            }
-            ++step;
             grid.robotClean();
+            grid.robotMoveSouth();
         }
     }
 }

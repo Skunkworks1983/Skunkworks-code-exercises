33d75ee7177c6026bb3cf55398a77d2384982b9a
Jack Alexander <jalexander.qed@gmail.com>
2026-01-14 18:22:44 -0800

    Finished solution.

--- roomba/java/Main.java
+++ roomba/java/Main.java
@@ -11,22 +11,33 @@ public class Main {
     static int step = 0;
 
     public static void moveRobot(Grid grid) {
-        int stepType = step % 4;
-        switch (stepType) {
-            case 0:
-                grid.robotMoveSouth();
-                break;
-            case 1:
-                grid.robotMoveEast();
-                break;
-            case 2:
-                grid.robotMoveNorth();
-                break;
-            case 3:
-                grid.robotMoveWest();
-                break;
-        }
-        ++step;
+        cleanDfs(grid);
+        System.exit(grid.allClean() ? 0 : 1);
+    }
+
+    public static void cleanDfs(Grid grid) {
         grid.robotClean();
+        Location robotLocation = grid.robotLocation();
+        Location south = new Location(robotLocation.x, robotLocation.y + 1);
+        Location north = new Location(robotLocation.x, robotLocation.y - 1);
+        Location east = new Location(robotLocation.x + 1, robotLocation.y);
+        Location west = new Location(robotLocation.x - 1, robotLocation.y);
+
+        if (!grid.isClean(south.x, south.y) && grid.robotMoveSouth()) {
+            cleanDfs(grid);
+            grid.robotMoveNorth();
+        }
+        if (!grid.isClean(north.x, north.y) && grid.robotMoveNorth()) {
+            cleanDfs(grid);
+            grid.robotMoveSouth();
+        }
+        if (!grid.isClean(east.x, east.y) && grid.robotMoveEast()) {
+            cleanDfs(grid);
+            grid.robotMoveWest();
+        }
+        if (!grid.isClean(west.x, west.y) && grid.robotMoveWest()) {
+            cleanDfs(grid);
+            grid.robotMoveEast();
+        }
     }
 }

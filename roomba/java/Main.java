5b6dbcc2fffd2f4054868ce88d7b0af353601499
Jack Alexander <jalexander.qed@gmail.com>
2026-01-14 18:30:12 -0800

    Improve the setup for the roomba problem and change naming in Main.java.

--- roomba/java/Main.java
+++ roomba/java/Main.java
@@ -3,41 +3,33 @@ public class Main {
         Grid grid = new Grid(10, 10);
         grid.print();
         grid.sleep();
-        while (!grid.allClean()) {
-            moveRobot(grid);
-        }
+
+        cleanFloor(grid);
+
+        System.exit(grid.allClean() ? 0 : 1);
     }
 
     static int step = 0;
 
-    public static void moveRobot(Grid grid) {
-        cleanDfs(grid);
-        System.exit(grid.allClean() ? 0 : 1);
-    }
-
-    public static void cleanDfs(Grid grid) {
-        grid.robotClean();
-        Location robotLocation = grid.robotLocation();
-        Location south = new Location(robotLocation.x, robotLocation.y + 1);
-        Location north = new Location(robotLocation.x, robotLocation.y - 1);
-        Location east = new Location(robotLocation.x + 1, robotLocation.y);
-        Location west = new Location(robotLocation.x - 1, robotLocation.y);
-
-        if (!grid.isClean(south.x, south.y) && grid.robotMoveSouth()) {
-            cleanDfs(grid);
-            grid.robotMoveNorth();
-        }
-        if (!grid.isClean(north.x, north.y) && grid.robotMoveNorth()) {
-            cleanDfs(grid);
-            grid.robotMoveSouth();
-        }
-        if (!grid.isClean(east.x, east.y) && grid.robotMoveEast()) {
-            cleanDfs(grid);
-            grid.robotMoveWest();
-        }
-        if (!grid.isClean(west.x, west.y) && grid.robotMoveWest()) {
-            cleanDfs(grid);
-            grid.robotMoveEast();
+    public static void cleanFloor(Grid grid) {
+        while (true) {
+            int stepType = step % 4;
+            switch (stepType) {
+                case 0:
+                    grid.robotMoveSouth();
+                    break;
+                case 1:
+                    grid.robotMoveEast();
+                    break;
+                case 2:
+                    grid.robotMoveNorth();
+                    break;
+                case 3:
+                    grid.robotMoveWest();
+                    break;
+            }
+            ++step;
+            grid.robotClean();
         }
     }
 }

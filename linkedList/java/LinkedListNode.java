45a42ce51fb6cdf6441a97e27d19b7c9c4a193ed
Jack Alexander <jalexander.qed@gmail.com>
2026-01-14 18:16:06 -0800

    Arlo or Luca's work on linked list.

---
+++ linkedList/java/LinkedListNode.java
@@ -0,0 +1,5 @@
+public class LinkedListNode {
+    public void print() {
+        System.out.println("foo");
+    }
+}
---
+++ linkedList/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main() {
+        LinkedListNode n = new LinkedListNode();
+        n.print();
+    }
+}
